48fb7b146293d503ec60d5d2b2a32cfee1ddd67f
Robin Kaiser <t@r-k.mx>
2020-05-01 21:59:23 +0200

    just starting out

---
+++ main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
+	"net/url"
+	"os"
+	"os/signal"
+	"time"
+)
+
+func main() {
+	listenAddrPtr := flag.String("port", "5000", "The required port")
+	listenAddr := fmt.Sprintf(":%v", *listenAddrPtr)
+	fmt.Printf("Server is ready to handle requests at port %v\n", *listenAddrPtr)
+
+	// here you could also go with third party packages to create a router
+	router := mux.NewRouter()
+
+	router.HandleFunc("/bitbucket-receive", bitBucketReceiveHandler).Methods("POST")
+	router.HandleFunc("/github-receive", gitHubReceiveHandler).Methods("POST")
+	router.HandleFunc("/gitlab-receive", gitLabReceiveHandler).Methods("POST")
+
+	router.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("pong"))
+	})
+
+	server := &http.Server{
+		Addr:         listenAddr,
+		Handler:      router,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  15 * time.Second,
+	}
+
+	done := make(chan bool, 1)
+	quit := make(chan os.Signal, 1)
+
+	signal.Notify(quit, os.Interrupt)
+
+	go func() {
+		<-quit
+		fmt.Println("Server is shutting down...")
+
+		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+		defer cancel()
+
+		server.SetKeepAlivesEnabled(false)
+		if err := server.Shutdown(ctx); err != nil {
+			fmt.Printf("Could not gracefully shutdown the server: %v\n", err)
+			os.Exit(-1)
+		}
+		close(done)
+	}()
+
+	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		fmt.Printf("Could not listen on %s: %v\n", listenAddr, err)
+		os.Exit(-1)
+	}
+
+	<-done
+	fmt.Println("Server stopped")
+}
+
+func bitBucketReceiveHandler(writer http.ResponseWriter, request *http.Request) {
+	var payload BitBucketPushPayload
+	err := json.NewDecoder(request.Body).Decode(&payload)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	queryParams, err := url.ParseQuery(request.URL.RawQuery)
+	if err != nil {
+		fmt.Println("Could not parse query params")
+		return
+	}
+
+	if len(queryParams["id"]) == 0 || len(queryParams["token"]) == 0 {
+		fmt.Println("Missing request parameters")
+		return
+	}
+
+	fmt.Println("id: " + queryParams["id"][0])
+	fmt.Println("token: " + queryParams["token"][0])
+
+	fmt.Println("branch: " + payload.Push.Changes[0].New.Name)
+	fmt.Println("repo full name: " + payload.Repository.FullName)
+
+	writer.WriteHeader(http.StatusOK)
+	writer.Write([]byte("received"))
+}
+
+func gitHubReceiveHandler(writer http.ResponseWriter, request *http.Request) {
+
+}
+
+func gitLabReceiveHandler(writer http.ResponseWriter, request *http.Request) {
+
+}
