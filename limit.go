66d32d2cda9aa0899495771ac6aab06187f0e2f1
Robin Kaiser <t@r-k.mx>
2020-08-22 01:19:51 +0200

    slightly increased rate limit

--- limit.go
+++ limit.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 )
 
-var limiter = rate.NewLimiter(10, 30)
+var limiter = rate.NewLimiter(15, 30)
 
 func limit(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
