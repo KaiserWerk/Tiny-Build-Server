0559af234dcf8cdca4f9e913569f6ec88c832dfb
Robin Kaiser <t@r-k.mx>
2020-09-06 14:22:12 +0200

    Create build.ps1

---
+++ build.ps1
@@ -0,0 +1,6 @@
+$sourcecode = "."
+$target = "build/tiny-build-server"
+$env:GOOS = 'windows'; $env:GOARCH = 'amd64';               go build -o "$($target)-win64.exe" $sourcecode
+$env:GOOS = 'linux';   $env:GOARCH = 'amd64';               go build -o "$($target)-linux64" $sourcecode
+$env:GOOS = 'linux';   $env:GOARCH = 'arm'; $env:GOARM=5;   go build -o "$($target)-raspi32" $sourcecode
+$env:GOOS = 'darwin';  $env:GOARCH = 'amd64';               go build -o "$($target)-macos64.macos" $sourcecode
