9955513c8701d8dc09ce7f371aa7b1f61549aa54
Robin Kaiser <t@r-k.mx>
2020-09-06 22:50:38 +0200

    added more site handlers

--- site_handler.go
+++ site_handler.go
@@ -322,6 +322,44 @@ func adminSettingsHandler(w http.ResponseWriter, r *http.Request) {
 
 func buildDefinitionListHandler(w http.ResponseWriter, r *http.Request) {
 
+
+	t := templates["build_definition_list.html"]
+	if t != nil {
+		err := t.Execute(w, nil)
+		if err != nil {
+			fmt.Println("error:", err.Error())
+		}
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+	}
+}
+
+func buildDefinitionAddHandler(w http.ResponseWriter, r *http.Request) {
+
+
+	t := templates["build_definition_add.html"]
+	if t != nil {
+		err := t.Execute(w, nil)
+		if err != nil {
+			fmt.Println("error:", err.Error())
+		}
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+	}
+}
+
+func buildDefinitionEditHandler(w http.ResponseWriter, r *http.Request) {
+
+
+	t := templates["build_definition_edit.html"]
+	if t != nil {
+		err := t.Execute(w, nil)
+		if err != nil {
+			fmt.Println("error:", err.Error())
+		}
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+	}
 }
 
 func buildDefinitionShowHandler(w http.ResponseWriter, r *http.Request) {
@@ -355,12 +393,75 @@ func buildDefinitionShowHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	var be buildExecution
+	var beList = make([]buildExecution, 0)
+	rows, err := db.Query("SELECT id, result, execution_time, executed_at FROM build_execution WHERE " +
+		"build_definition_id = ? ORDER BY executed_at DESC", bd.Id)
+	if err != nil {
+		writeToConsole("could not fetch most recent build executions: " + err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	for rows.Next() {
+		err = rows.Scan(&be.Id, &be.Result, &be.ExecutionTime, &be.ExecutedAt)
+		if err != nil {
+			writeToConsole("could not scan build execution: " + err.Error())
+			continue
+		}
+		beList = append(beList, be)
+		be = buildExecution{}
+	}
+
+	failedBuildCount := 0
+	successBuildCount := 0
+	avg := 0.0
+	i := 0
+	for _, v := range beList {
+		if v.Result == "success" {
+			successBuildCount++
+		}
+		if v.Result == "failed" {
+			failedBuildCount++
+		}
+		avg += v.ExecutionTime
+		i++
+	}
+	//writeToConsole("total execution time: " + fmt.Sprintf("%v in %v iterations", avg, i))
+	avg = avg / float64(i)
+
+
+
+	successRate := float64(successBuildCount) / float64(i) * 100
+
+
+	var recentExecutions []buildExecution
+	if len(beList) >= 5 {
+		recentExecutions = beList[:5]
+	} else {
+		for _, v := range beList {
+			recentExecutions = append(recentExecutions, v)
+		}
+	}
+
 	data := struct {
 		BuildDefinition buildDefinition
+		RecentExecutions []buildExecution
 		CurrentUser user
+		TotalBuildCount int
+		FailedBuildCount int
+		SuccessBuildCount int
+		SuccessRate	string
+		AvgRuntime string
 	} {
 		BuildDefinition: bd,
+		RecentExecutions: recentExecutions,
 		CurrentUser: currentUser,
+		TotalBuildCount: len(beList),
+		FailedBuildCount: failedBuildCount,
+		SuccessBuildCount: successBuildCount,
+		SuccessRate: fmt.Sprintf("%.2f", successRate),
+		AvgRuntime: fmt.Sprintf("%.2f", avg),
 	}
 
 	t := templates["build_definition_show.html"]
@@ -375,12 +476,18 @@ func buildDefinitionShowHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func buildDefinitionEditHandler(w http.ResponseWriter, r *http.Request) {
-
-}
-
 func buildDefinitionRemoveHandler(w http.ResponseWriter, r *http.Request) {
 
+
+	t := templates["build_definition_remove.html"]
+	if t != nil {
+		err := t.Execute(w, nil)
+		if err != nil {
+			fmt.Println("error:", err.Error())
+		}
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+	}
 }
 
 func buildDefinitionListExecutionsHandler(w http.ResponseWriter, r *http.Request) {
@@ -389,9 +496,29 @@ func buildDefinitionListExecutionsHandler(w http.ResponseWriter, r *http.Request
 
 func buildExecutionListHandler(w http.ResponseWriter, r *http.Request) {
 
+
+	t := templates["build_execution_list.html"]
+	if t != nil {
+		err := t.Execute(w, nil)
+		if err != nil {
+			fmt.Println("error:", err.Error())
+		}
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+	}
 }
 
 func buildExecutionShowHandler(w http.ResponseWriter, r *http.Request) {
 
+
+	t := templates["build_execution_show.html"]
+	if t != nil {
+		err := t.Execute(w, nil)
+		if err != nil {
+			fmt.Println("error:", err.Error())
+		}
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+	}
 }
 
