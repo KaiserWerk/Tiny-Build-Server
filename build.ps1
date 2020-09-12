4126c455038bc0e26e1591741e76862f16ad383b
Robin Kaiser <t@r-k.mx>
2020-09-12 02:26:58 +0200

    changed the build file

--- build.ps1
+++ build.ps1
@@ -4,3 +4,4 @@ $env:GOOS = 'windows'; $env:GOARCH = 'amd64';               go build -o "$($targ
 $env:GOOS = 'linux';   $env:GOARCH = 'amd64';               go build -o "$($target)-linux64" $sourcecode
 $env:GOOS = 'linux';   $env:GOARCH = 'arm'; $env:GOARM=5;   go build -o "$($target)-raspi32" $sourcecode
 $env:GOOS = 'darwin';  $env:GOARCH = 'amd64';               go build -o "$($target)-macos64.macos" $sourcecode
+# -ldflags "-X main.version=0.0.1,main.commitHash=123abc"
