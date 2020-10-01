b69da52d1427f87fc4515aaa064e7931e7c88c30
Robin Kaiser <t@r-k.mx>
2020-10-01 07:23:55 +0200

    improved build file

--- build.ps1
+++ build.ps1
@@ -1,8 +1,9 @@
 $sourcecode = "."
 $target = "build/tiny-build-server"
-$env:GOOS = 'windows'; $env:GOARCH = 'amd64';               go build -o "$($target)-win64.exe" $sourcecode
-$env:GOOS = 'linux';   $env:GOARCH = 'amd64';               go build -o "$($target)-linux64" $sourcecode
-$env:GOOS = 'linux';   $env:GOARCH = 'arm'; $env:GOARM=5;   go build -o "$($target)-raspi32" $sourcecode
-$env:GOOS = 'darwin';  $env:GOARCH = 'amd64';               go build -o "$($target)-macos64.macos" $sourcecode
-# -ldflags "-X main.version=0.0.1,main.commitHash=123abc"
+$version = "1.0.0"
+$date = Get-Date -Format "yyyy-MM-dd HH:mm:ss K"
+$env:GOOS = 'windows'; $env:GOARCH = 'amd64';             go build -o "$($target)-win64.exe" -ldflags "-s -w -X 'main.version=$($version)' -X 'main.versionDate=$($date)'" $sourcecode
+$env:GOOS = 'linux';   $env:GOARCH = 'amd64';             go build -o "$($target)-linux64"   -ldflags "-s -w -X 'main.version=$($version)' -X 'main.versionDate=$($date)'" $sourcecode
+$env:GOOS = 'linux';   $env:GOARCH = 'arm'; $env:GOARM=5; go build -o "$($target)-raspi32"   -ldflags "-s -w -X 'main.version=$($version)' -X 'main.versionDate=$($date)'" $sourcecode
+$env:GOOS = 'darwin';  $env:GOARCH = 'amd64';             go build -o "$($target)-macos64"   -ldflags "-s -w -X 'main.version=$($version)' -X 'main.versionDate=$($date)'" $sourcecode
 # git rev-parse --verify HEAD
