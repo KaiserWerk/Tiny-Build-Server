c343688d0ead40e1147075ae86177841466cf754
Robin Kaiser <t@r-k.mx>
2020-10-01 22:36:05 +0200

    Update build_steps_golang.go

--- build_steps_golang.go
+++ build_steps_golang.go
@@ -3,25 +3,24 @@ package main
 import (
 	"github.com/stvp/slug"
 	"os/exec"
+	"runtime"
 	"strings"
 )
 
 type golangBuildDefinition buildDefinition
-
 
 func (bd golangBuildDefinition) runTests(messageCh chan string) error {
 
 	return nil
 }
 
-func (bd golangBuildDefinition) runBenchmarkTests(messageCh chan string) error {
+func (bd golangBuildDefinition) runBenchmarkTests(smessageCh chan string) error {
 
 	return nil
 }
 
-func (bd golangBuildDefinition) buildArtifact(messageCh chan string, projectDir string) error {
+func (bd golangBuildDefinition) buildArtifact(messageCh chan string, projectDir string) (string, error) {
 	cloneDir := projectDir + "/clone"
-	buildDir := projectDir + "/build"
 	artifactDir := projectDir + "/artifact"
 
 	slug.Replacement = '-'
@@ -30,16 +29,28 @@ func (bd golangBuildDefinition) buildArtifact(messageCh chan string, projectDir 
 		binaryName += ".exe"
 	}
 	messageCh <- "binary name set to " + binaryName
-	cmd := exec.Command("")
 
-	// Command
-	// go build -o <output> -a -v -work -x -ldflags "-s -w -X main.<Var>=<Val>" <input>
+	// format all go files?
 
+	var dateStr string
+	if runtime.GOOS == "windows" {
+		dateStr = `Get-Date -Format "yyyy-MM-dd HH:mm:ss K"`
+	} else {
+		dateStr = `date -u +"%Y-%m-%d %H:%M:%S %:z"`
+	}
+	artifact := artifactDir + "/" + binaryName
+	buildCommand := `build -o "<output>" -a -v -work -x -ldflags "-s -w -X main.versionDate=` + dateStr + `" <input>`
+	buildCommand = strings.Replace(buildCommand, "<output>", artifact, 1)
+	buildCommand = strings.Replace(buildCommand, "<input>", cloneDir, 1)
 
-	return nil
-}
+	cmd := exec.Command("go", strings.Split(buildCommand, " ")...)
+	messageCh <- "build command to be executed: '" + cmd.String() + "'"
+	result, err := cmd.Output()
+	if err != nil {
+		messageCh <- "build was not successful: " + err.Error()
+		return "", err
+	}
+	messageCh <- "build successful: " + string(result)
 
-func (bd golangBuildDefinition) applyMigrations(messageCh chan string) error {
-
-	return nil
+	return artifact, nil
 }
