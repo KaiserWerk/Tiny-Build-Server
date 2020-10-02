3c9d756a758a87baf3fb81f89eb1c85a0ee3c06e
Robin Kaiser <t@r-k.mx>
2020-10-02 13:14:10 +0200

    Update build_steps_golang.go

--- build_steps_golang.go
+++ build_steps_golang.go
@@ -14,7 +14,7 @@ func (bd golangBuildDefinition) runTests(messageCh chan string) error {
 	return nil
 }
 
-func (bd golangBuildDefinition) runBenchmarkTests(smessageCh chan string) error {
+func (bd golangBuildDefinition) runBenchmarkTests(messageCh chan string) error {
 
 	return nil
 }
@@ -39,11 +39,13 @@ func (bd golangBuildDefinition) buildArtifact(messageCh chan string, projectDir 
 		dateStr = `date -u +"%Y-%m-%d %H:%M:%S %:z"`
 	}
 	artifact := artifactDir + "/" + binaryName
+	// set env vars!!!!!!!!!!!!
 	buildCommand := `build -o "<output>" -a -v -work -x -ldflags "-s -w -X main.versionDate=` + dateStr + `" <input>`
 	buildCommand = strings.Replace(buildCommand, "<output>", artifact, 1)
 	buildCommand = strings.Replace(buildCommand, "<input>", cloneDir, 1)
 
 	cmd := exec.Command("go", strings.Split(buildCommand, " ")...)
+
 	messageCh <- "build command to be executed: '" + cmd.String() + "'"
 	result, err := cmd.Output()
 	if err != nil {
