9acf073e809453d89521a0f4b45ef5f8bfb08d5c
Robin Kaiser <t@r-k.mx>
2020-11-10 10:59:39 +0100

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,9 +1,13 @@
-$sourcecode = "."
+
+$sourcecode = "cmd/tiny-build-server/main.go"
 $target = "build/tiny-build-server"
-$version = "1.0.0"
+$version = "0.0.1"
 $date = Get-Date -Format "yyyy-MM-dd HH:mm:ss K"
+# Windows, 64-bit
 $env:GOOS = 'windows'; $env:GOARCH = 'amd64';             go build -o "$($target)-win64.exe" -ldflags "-s -w -X 'main.version=$($version)' -X 'main.versionDate=$($date)'" $sourcecode
+# Linux, 64-bit
 $env:GOOS = 'linux';   $env:GOARCH = 'amd64';             go build -o "$($target)-linux64"   -ldflags "-s -w -X 'main.version=$($version)' -X 'main.versionDate=$($date)'" $sourcecode
+# Raspberry PI
 $env:GOOS = 'linux';   $env:GOARCH = 'arm'; $env:GOARM=5; go build -o "$($target)-raspi32"   -ldflags "-s -w -X 'main.version=$($version)' -X 'main.versionDate=$($date)'" $sourcecode
+# Mac OSX
 $env:GOOS = 'darwin';  $env:GOARCH = 'amd64';             go build -o "$($target)-macos64"   -ldflags "-s -w -X 'main.version=$($version)' -X 'main.versionDate=$($date)'" $sourcecode
-# git rev-parse --verify HEAD
