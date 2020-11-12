7795cc99bbbd4fdfd7f38878c1073ad4aa911512
Robin Kaiser <t@r-k.mx>
2020-11-12 20:35:13 +0100

    Create enums.go

---
+++ internal/helper/enums.go
@@ -0,0 +1,10 @@
+package helper
+
+const (
+	AccountCreated = "account_created"
+	AccountLocked = "account_locked"
+	ConfirmRegistration = "confirm_registration"
+	PasswordReset = "password_reset"
+	RegistrationConfirmed = "registration_confirmed"
+	ResetPassword = "reset_password"
+)
