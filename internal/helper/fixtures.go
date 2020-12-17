75bb79d6646aa2e40c4fde821f704cd2eaf38b33
Robin Kaiser <t@r-k.mx>
2020-12-17 08:50:25 +0100

    improvement: better email subject lines

--- internal/helper/fixtures.go
+++ internal/helper/fixtures.go
@@ -1,10 +1,10 @@
 package helper
 
 var EmailSubjects = map[EmailMessageType]string{
-	AccountCreated: "Build Server: Your account was created",
-	AccountLocked: "Build Server: Your account has been locked",
-	ConfirmRegistration: "Build Server: Please confirm your registration",
-	PasswordReset: "Build Server: Instructions on how to reset your password",
-	RegistrationConfirmed: "Build Server: Your registration was successfully confirmed",
-	ResetPassword: "Build Server: Your password has been reset",
+	AccountCreated: "Tiny Build Server: Your account was created",
+	AccountLocked: "Tiny Build Server: Your account has been locked",
+	ConfirmRegistration: "Tiny Build Server: Please confirm your registration",
+	PasswordReset: "Tiny Build Server: Instructions on how to reset your password",
+	RegistrationConfirmed: "Tiny Build Server: Your registration was successfully confirmed",
+	ResetPassword: "Tiny Build Server: Your password has been reset",
 }
