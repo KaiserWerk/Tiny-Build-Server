4ec9ed0076a0c4ca0cf9e997c7e9594d5aff185e
Robin Kaiser <t@r-k.mx>
2020-12-18 22:52:06 +0100

    update: added type name suffix for clarity

--- internal/helper/enums.go
+++ internal/helper/enums.go
@@ -3,11 +3,12 @@ package helper
 type EmailMessageType string
 
 const (
-	AccountCreated        EmailMessageType = "account_created"
-	AccountLocked         EmailMessageType = "account_locked"
-	ConfirmRegistration   EmailMessageType = "confirm_registration"
-	PasswordReset         EmailMessageType = "password_reset"
-	RegistrationConfirmed EmailMessageType = "registration_confirmed"
-	ResetPassword         EmailMessageType = "reset_password"
+	AccountCreatedEmail        EmailMessageType = "account_created"
+	AccountLockedEmail         EmailMessageType = "account_locked"
+	ConfirmRegistrationEmail   EmailMessageType = "confirm_registration"
+	RequestNewPasswordEmail    EmailMessageType = "request_new_password"
+	RegistrationConfirmedEmail EmailMessageType = "registration_confirmed"
+	ConfirmPasswordResetEmail  EmailMessageType = "confirm_password_reset"
 )
+
 
