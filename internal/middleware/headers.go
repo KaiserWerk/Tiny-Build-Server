ab0c154eacce09ee1739431c47203b54bbfa071d
Robin Kaiser <t@r-k.mx>
2020-12-18 22:52:43 +0100

    improvement: added middleware to set headers globally

---
+++ internal/middleware/headers.go
@@ -0,0 +1,16 @@
+package middleware
+
+import (
+	"net/http"
+)
+
+
+func SetHeaders(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Cache-Control", "max-age=0, no-cache, no-store, must-revalidate")
+		w.Header().Set("Pragma", "no-cache")
+		w.Header().Set("Expires", "Thu, 01 Jan 1970 00:00:00 GMT")
+
+		next.ServeHTTP(w, r)
+	})
+}
