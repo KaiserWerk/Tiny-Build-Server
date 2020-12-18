cc381e5b9c4aa162658302ab0b4d4a42845d20fa
Robin Kaiser <t@r-k.mx>
2020-12-18 22:52:21 +0100

    update: added suffix for clarity

--- internal/helper/fixtures.go
+++ internal/helper/fixtures.go
@@ -1,10 +1,10 @@
 package helper
 
 var EmailSubjects = map[EmailMessageType]string{
-	AccountCreated: "Tiny Build Server: Your account was created",
-	AccountLocked: "Tiny Build Server: Your account has been locked",
-	ConfirmRegistration: "Tiny Build Server: Please confirm your registration",
-	PasswordReset: "Tiny Build Server: Instructions on how to reset your password",
-	RegistrationConfirmed: "Tiny Build Server: Your registration was successfully confirmed",
-	ResetPassword: "Tiny Build Server: Your password has been reset",
+	AccountCreatedEmail:        "Tiny Build Server: Your account was created",
+	AccountLockedEmail:         "Tiny Build Server: Your account has been locked",
+	ConfirmRegistrationEmail:   "Tiny Build Server: Please confirm your registration",
+	RequestNewPasswordEmail:    "Tiny Build Server: Instructions on how to reset your password",
+	RegistrationConfirmedEmail: "Tiny Build Server: Your registration was successfully confirmed",
+	ConfirmPasswordResetEmail:  "Tiny Build Server: Your password has been reset",
 }
