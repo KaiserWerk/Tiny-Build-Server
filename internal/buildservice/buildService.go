2920a39090a26e4f0238d3b8795e398e9ad05235
Robin Kaiser <web@r-k.mx>
2021-11-12 13:57:22 +0100

    add function getCurrentVersionTag()

--- internal/buildservice/buildService.go
+++ internal/buildservice/buildService.go
@@ -638,3 +638,18 @@ func CheckPayloadHeader(content entity.BuildDefinitionContent, r *http.Request) 
 
 	return nil
 }
+
+func getCurrentVersionTag() string {
+	cmd := exec.Command("git", "tag", "-l", "--sort=-version:refname")
+	output, err := cmd.Output()
+	if err != nil {
+		return ""
+	}
+
+	versions := strings.Split(strings.ReplaceAll(strings.TrimSpace(string(output)), "\r\n", "\n"), "\n")
+	if len(versions) > 0 {
+		return versions[0]
+	}
+
+	return ""
+}
